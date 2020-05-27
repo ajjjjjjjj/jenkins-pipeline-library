cd7981fab681294a67ec831bcfe625c449134a48
kozakj <jan.kozak@contractors.roche.com>
2020-05-27 13:46:59 +0200

    use Map param

--- vars/janek.groovy
+++ vars/janek.groovy
@@ -1,7 +1,7 @@
-def call(Map map) {
+def call(Map config) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
     node {
-        echo "Global variable example, ${map.name}."
+        echo "Global variable example, ${config.name}."
     }
 }
