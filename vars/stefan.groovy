552acc20993938a253e6210b22136055e066017e
kozakj <jan.kozak@contractors.roche.com>
2020-05-27 15:11:41 +0200

    see what is inside script

--- vars/stefan.groovy
+++ vars/stefan.groovy
@@ -1,5 +1,5 @@
 def call(script, name) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-    echo "Env: ${script.env.DB_ENGINE}, def: ${script.getProperty('EXAMPLE')}, Global variable example, ${name}."
+    echo "Env: ${script.env.DB_ENGINE}, def: ${EXAMPLE}, Global variable example, ${name}."
 }
