7f63235371620df971ef1a961845c571e91941a3
kozakj <jan.kozak@contractors.roche.com>
2020-05-27 15:48:51 +0200

    refactored stefan

--- vars/stefan.groovy
+++ vars/stefan.groovy
@@ -1,5 +1,5 @@
-def call(script, name) {
+def call(name) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-    echo "Env: ${script.env.DB_ENGINE}, def: ${DISABLE_AUTH}, Global variable example, ${name}."
+    echo "Env: ${DB_ENGINE}, def: ${DISABLE_AUTH}, Global variable example, ${name}."
 }
