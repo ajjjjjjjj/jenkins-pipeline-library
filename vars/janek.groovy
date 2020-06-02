4397284a0f4818340d8a7b6ff11438ccd7afc73e
kozakj <jan.kozak@contractors.roche.com>
2020-06-02 13:02:58 +0200

    modify DB_ENGINE

--- vars/janek.groovy
+++ vars/janek.groovy
@@ -1,6 +1,7 @@
 def call(Map config = [name:'Janek']) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
+    DB_ENGINE = "hahah"
     node {
         echo "Global variable example, ${config.name}."
     }
