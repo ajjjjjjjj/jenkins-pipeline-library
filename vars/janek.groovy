1ee5b08715a6727ea7a980666b1072da624196ec
kozakj <jan.kozak@contractors.roche.com>
2020-06-02 13:13:43 +0200

    VERSION test2

--- vars/janek.groovy
+++ vars/janek.groovy
@@ -1,9 +1,8 @@
 def call(Map config = [name:'Janek']) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-    environment {
-        VERSION = ":)"
-    }
+    env.VERSION = ":)"
+
     node {
         echo "Global variable example, ${config.name}."
     }
