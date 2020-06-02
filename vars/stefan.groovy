414584e9652c2daa325517ff6136473541cde6c5
kozakj <jan.kozak@contractors.roche.com>
2020-06-02 12:33:23 +0200

    removed stefan param

--- vars/stefan.groovy
+++ vars/stefan.groovy
@@ -1,7 +1,7 @@
-def call(name) {
+def call() {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
     script {
-        echo "Env: ${DB_ENGINE}, def: ${DISABLE_AUTH}, Global variable example, ${name}."
+        echo "Env: ${DB_ENGINE}, def: ${DISABLE_AUTH}, Global variable example."
     }
 }
