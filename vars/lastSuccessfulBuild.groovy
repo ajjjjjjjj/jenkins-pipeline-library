2f1e4d5f97dda2866bea690363ba6d74b8e1471e
kozakj <jan.kozak@contractors.roche.com>
2020-06-03 12:58:11 +0200

    added lastSuccessfulBuild return value

--- vars/lastSuccessfulBuild.groovy
+++ vars/lastSuccessfulBuild.groovy
@@ -1,17 +1,20 @@
 def call() {
     script{
-        def lastSuccessfulBuildID = 0
-        def build = currentBuild.previousSuccessfulBuild
+        def lastSuccessfulBuildDescription = 0
+        def build = currentBuild.previousBuild
         while (build != null) {
             if (build.result == "SUCCESS")
             {
-                lastSuccessfulBuildID = build.id as Integer
+//                lastSuccessfulBuildID = build.id as Integer
                 lastSuccessfulBuildDescription = build.description
                 break
             }
             build = build.previousBuild
         }
+        println "lastSuccessfulBuildDescription: ${lastSuccessfulBuildDescription}"
         println "previousSuccessfulBuild id: ${currentBuild.previousSuccessfulBuild.id}"
         println "previousSuccessfulBuild desc: ${currentBuild.previousSuccessfulBuild.description}"
+
+        return lastSuccessfulBuildDescription;
     }
 }
