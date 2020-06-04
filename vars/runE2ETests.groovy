3b360c29250dbd657ab2dfb58cb1716c6d353dbf
kozakj <jan.kozak@contractors.roche.com>
2020-06-04 10:49:20 +0200

    aded pipeline to runE2ETests

--- vars/runE2ETests.groovy
+++ vars/runE2ETests.groovy
@@ -1,15 +1,21 @@
 def call(version) {
+
     log.info("runE2ETests, version" + version)
 
-    stages {
-        stage('docker pull') {
-            steps {
-                echo 'Docker pull...'
+    pipeline {
+
+        agent none
+
+        stages {
+            stage('docker pull') {
+                steps {
+                    echo 'Docker pull...'
+                }
             }
-        }
-        stage('run e2e') {
-            steps {
-                echo 'run e2e...'
+            stage('run e2e') {
+                steps {
+                    echo 'run e2e...'
+                }
             }
         }
     }
