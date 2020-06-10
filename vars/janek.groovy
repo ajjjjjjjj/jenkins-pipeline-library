d9c194df7ca83a5e255803f2a1ba971d636f5d1c
kozakj <jan.kozak@contractors.roche.com>
2020-06-10 11:37:46 +0200

    added map with dynamic string build

--- vars/janek.groovy
+++ vars/janek.groovy
@@ -1,7 +1,7 @@
 def call(Map config = [:]) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
-    env.VERSION = ":)"
+    VERSION = ":)"
 
     node {
         echo "Global variable example, ${config.name}, ${config.env}."
