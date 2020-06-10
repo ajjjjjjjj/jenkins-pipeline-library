74a743303e1c436a4bff689eeb09e90ed550825f
kozakj <jan.kozak@contractors.roche.com>
2020-06-10 12:17:19 +0200

    fixed global env setting

--- vars/janek.groovy
+++ vars/janek.groovy
@@ -2,7 +2,7 @@ def call(Map config = [:]) {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
     env.VERSION = ":)"
-
+    echo "VERSION is set to $VERSION"
     node {
         echo "Global variable example, ${config.name}, ${config.env}."
     }
